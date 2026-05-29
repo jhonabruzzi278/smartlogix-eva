b2d48382adccd00fe86cdfa02f38b871ffa39e59
JONAHBRUZZI <jon.guerra@duocuc.cl>
2026-05-28 23:26:15 -0400

    Auditoria final: limpiar leftovers Java, prod.yml Node, dead SHIPPING_QUEUE

--- Backend/init-db.sql
+++ Backend/init-db.sql
@@ -2,4 +2,3 @@ CREATE DATABASE orders_db;
 CREATE DATABASE inventory_db;
 CREATE DATABASE shipping_db;
 CREATE DATABASE notification_db;
-CREATE DATABASE identity_db;
--- Backend/init-sqs.sh/bootstrap.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-awslocal sqs create-queue --queue-name orders-queue
-awslocal sqs create-queue --queue-name shipping-queue
-awslocal sqs create-queue --queue-name notification-events-queue
